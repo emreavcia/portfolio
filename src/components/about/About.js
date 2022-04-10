b80c39f418da6240d7bcce9687f8d194d8a07c3f
emreavcia <avciemrea@gmail.com>
2022-04-11 00:36:37 +0200

    about page is more functional now

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,17 +1,67 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.development'
+import { v4 as uuid } from 'uuid'
 import "./about.css"
 
 const About = () => {
 
+    const careerContent = [
+        {
+            id: uuid(),
+            heading: "Career",
+            content: "Third Secretary in MFA, TURKEY"
+        },
+        {
+            id: uuid(),
+            heading: "Education",
+            content: "Mechanical Engineering, Yildiz Technical University"
+        },
+        {
+            id: uuid(),
+            heading: "Currently",
+            content: "Powercoders Participant"
+        },
+        {
+            id: uuid(),
+            heading: "Career",
+            content: "Third Secretary-Vice Consul, Turkish Consulate General in Hargeisa"
+        },
+        {
+            id: uuid(),
+            heading: "Courses",
+            content: "HiCoders Full-Stack Programming 06.11.2021 -..."
+        },
+        {
+            id: uuid(),
+            heading: "Certificates",
+            content: "21 certifactes about Front-end Programming"
+        }
+    ]
 
+    const [counter, setCounter] = useState(1)
+    const [newCareerContent, setNewCareerContent] = useState([])
+
+    const handlePopUp = () => {
+        counter < careerContent.length && setCounter(counter + 1)
+        const content = careerContent.splice(0, `${counter}`)
+        setNewCareerContent(content)
+
+    }
 
     return (
         <section className='about'>
-
-            <h2>Aha! This is me</h2>
-
-            <img src="mySnapShot.jpg" alt="mySnapShot" />
-
+            <div className='click_me_section'>
+                <h2>Click on me <br /> (my picture)!</h2>
+                <img src="mySnapShot.jpg" alt="mySnapShot" onClick={handlePopUp} />
+            </div>
+            {
+                newCareerContent.map((item, index) => (
+                    <div key={item.id} className={`popup popup${index + 1}`}>
+                        <h3>{item.heading}</h3>
+                        <p>{item.content}</p>
+                    </div>
+                ))
+            }
         </section>
     )
 }
