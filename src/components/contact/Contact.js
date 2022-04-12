d4af54b6dae86630b4359f8f35cfbba786024418
emreavcia <avciemrea@gmail.com>
2022-04-12 07:27:21 +0200

    many changes are made

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,7 +5,10 @@ const Contact = () => {
     return (
         <section className='contact_me_container'>
             <h2 className='contact_me_heading'>Contact Me</h2>
-            <p className='contact_me_paragraph'>Drop me a line if you fancy a chat</p>
+            <div>
+                <p className='contact_me_paragraph'>I am available for a work</p>
+                <p className='contact_me_paragraph'>Drop me a line if you fancy a chat</p>
+            </div>
             <h3 className='contact_me_email'><a href="mailto:emre.avci@powercoders.org">emre.avci@powercoders.org</a></h3>
         </section>
     )
--- src/components/navigation/NavLinks.js
+++ src/components/navigation/NavLinks.js
@@ -6,9 +6,8 @@ const NavLinks = () => {
     return (
         <ul>
             <li> <Link className='navLinks' to="/about" >About</Link> </li>
-            <li> <Link className='navLinks' to="/Work" >Work</Link> </li>
             <li> <Link className='navLinks' to="/" >Joy</Link> </li>
-            <li> <Link className='navLinks' to="/Contact" >Contact</Link> </li>
+            <li> <Link className='navLinks' to="/contact" >Contact</Link> </li>
         </ul>
     )
 }
