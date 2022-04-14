0fbf31aa2495bb694eb13db621e0bd88f961590e
emreavcia <avciemrea@gmail.com>
2022-04-14 11:59:03 +0200

    footer margin left is changed to  5em

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <footer>
             <ul>
-                <li><a href="https://github.com/emreavcia" target="_blank" rel="noopener noreferrer"><SiGithub /></a> </li>
+                <li><a href="https://github.com/emreavcia/portfolio" target="_blank" rel="noopener noreferrer"><SiGithub /></a> </li>
                 <li><a href="https://www.linkedin.com/in/emre-avci-765888220/" target="_blank" rel="noopener noreferrer"><SiLinkedin /></a></li>
             </ul>
         </footer>
