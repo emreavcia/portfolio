08d23679d84d30aaee6c8817930ab2176db354b7
emreavcia <avciemrea@gmail.com>
2022-04-14 16:46:15 +0200

    butified navigation with active class using useState

--- src/components/joy/MainText.js
+++ src/components/joy/MainText.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./mainText.css"
 
 const MainText = () => {
+
     return (
         <>
             <p className='main_text'>I enjoy playing around with <span>HTML5</span>, <span>CSS3</span>, <span>JavaScript</span>, and <span>REACT.js.</span></p>
--- src/components/navigation/NavLinks.js
+++ src/components/navigation/NavLinks.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import "./navLinks.css"
 import { Link } from "react-router-dom"
+import { useState } from 'react'
+import { v4 as uuid } from 'uuid'
 
 const NavLinks = () => {
+
+    const [navLink, setNavLink] = useState([
+        {
+            id: uuid(),
+            navLinkName: "About",
+            navLinkTo: "/about",
+            active: false
+        },
+        {
+            id: uuid(),
+            navLinkName: "Joy",
+            navLinkTo: "/",
+            active: true
+        },
+        {
+            id: uuid(),
+            navLinkName: "Contact",
+            navLinkTo: "/contact",
+            active: false
+        }
+
+    ])
+
+    const handleNavLinkClikc = (id) => {
+        const newNavLinks = navLink.map(navLink => navLink.id !== id ? { ...navLink, active: false } : { ...navLink, active: true })
+        setNavLink(newNavLinks)
+    }
+
     return (
         <ul className='nav-list'>
-            <li className='nav-list-item'> <Link className='navLinks' to="/about" >About</Link> </li>
-            <li className='nav-list-item'> <Link className='navLinks' to="/" >Joy</Link> </li>
-            <li className='nav-list-item'> <Link className='navLinks' to="/contact" >Contact</Link> </li>
+            {
+                navLink.map(navLink => (
+                    <li className='nav-list-item'><Link onClick={() => handleNavLinkClikc(navLink.id)} className={`navLinks ${navLink.active && "navLinks-active"}`} to={navLink.navLinkTo}>{navLink.navLinkName}</Link></li>
+                ))
+            }
         </ul>
     )
 }
