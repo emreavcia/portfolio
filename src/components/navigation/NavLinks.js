776cfef9a19df8b3c7f5de1ec733a0782a3036c9
emreavcia <avciemrea@gmail.com>
2022-04-14 21:25:47 +0200

    logo added with click handler to change nav links state

--- src/components/navigation/NavLinks.js
+++ src/components/navigation/NavLinks.js
@@ -1,43 +1,14 @@
 import React from 'react'
 import "./navLinks.css"
 import { Link } from "react-router-dom"
-import { useState } from 'react'
-import { v4 as uuid } from 'uuid'
 
-const NavLinks = () => {
-
-    const [navLink, setNavLink] = useState([
-        {
-            id: uuid(),
-            navLinkName: "About",
-            navLinkTo: "/about",
-            active: false
-        },
-        {
-            id: uuid(),
-            navLinkName: "Joy",
-            navLinkTo: "/",
-            active: true
-        },
-        {
-            id: uuid(),
-            navLinkName: "Contact",
-            navLinkTo: "/contact",
-            active: false
-        }
-
-    ])
-
-    const handleNavLinkClikc = (id) => {
-        const newNavLinks = navLink.map(navLink => navLink.id !== id ? { ...navLink, active: false } : { ...navLink, active: true })
-        setNavLink(newNavLinks)
-    }
+const NavLinks = ({ navLink, handleNavLinkClick }) => {
 
     return (
         <ul className='nav-list'>
             {
                 navLink.map(navLink => (
-                    <li key={navLink.id} className='nav-list-item'><Link onClick={() => handleNavLinkClikc(navLink.id)} className={`navLinks ${navLink.active && "navLinks-active"}`} to={navLink.navLinkTo}>{navLink.navLinkName}</Link></li>
+                    <li key={navLink.id} className='nav-list-item'><Link onClick={() => handleNavLinkClick(navLink.id)} className={`navLinks ${navLink.active && "navLinks-active"}`} to={navLink.navLinkTo}>{navLink.navLinkName}</Link></li>
                 ))
             }
         </ul>
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -3,9 +3,35 @@ import "./navigation.css"
 import { Link } from "react-router-dom"
 import HamburgerMenu from './HamburgerMenu'
 import NavLinks from './NavLinks'
+import { v4 as uuid } from 'uuid'
 
 function Navigation() {
 
+  const [navLink, setNavLink] = useState([
+    {
+      id: uuid(),
+      navLinkName: "About",
+      navLinkTo: "/about",
+      active: false
+    },
+    {
+      id: uuid(),
+      navLinkName: "Joy",
+      navLinkTo: "/",
+      active: true
+    },
+    {
+      id: uuid(),
+      navLinkName: "Contact",
+      navLinkTo: "/contact",
+      active: false
+    }
+  ])
+
+  const handleNavLinkClick = (id) => {
+    const newNavLinks = navLink.map(navLink => navLink.id !== id ? { ...navLink, active: false } : { ...navLink, active: true })
+    setNavLink(newNavLinks)
+  }
   //to set the nav links to display and vice verse 
   const [displayNavLinks, setDisplayNavLinks] = useState(false)
 
@@ -18,12 +44,17 @@ function Navigation() {
     open === "hamburger" ? setOpen("cross") : setOpen("hamburger")
   }
 
+  const handleLogoClick = (extention) => {
+    const newNavLinks = navLink.map(navLink => navLink.navLinkTo !== extention ? { ...navLink, active: false } : { ...navLink, active: true })
+    setNavLink(newNavLinks)
+  }
+
   return (
     <>
       <header>
-        <Link to="/" className='logo'>EA</Link>
+        <Link to="/" className='logo' onClick={() => handleLogoClick("/")}>EA</Link>
         <nav>
-          {displayNavLinks && <NavLinks onClick={handleHamburgerMenuClick} />}
+          {displayNavLinks && <NavLinks onClick={handleHamburgerMenuClick} navLink={navLink} handleNavLinkClick={handleNavLinkClick} />}
           <HamburgerMenu
             handleHamburgerMenuClick={handleHamburgerMenuClick}
             open={open}
