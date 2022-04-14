f2a28b9bff9e5318e8a8a8dade52604c1329741f
emreavcia <avciemrea@gmail.com>
2022-04-14 22:18:53 +0200

    some minor changes are made

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,8 +6,8 @@ const Contact = () => {
         <section className='contact_me_container'>
             <h2 className='contact_me_heading'>Contact Me</h2>
             <div>
-                <p className='contact_me_paragraph'>I am available for a work</p>
-                <p className='contact_me_paragraph'>Drop me a line if you fancy a chat</p>
+                <p className='contact_me_paragraph'>I am available for a work.</p>
+                <p className='contact_me_paragraph'>Drop me a line if you fancy a chat.</p>
             </div>
             <h3 className='contact_me_email'><a href="mailto:emre.avci@powercoders.org">emre.avci@powercoders.org</a></h3>
         </section>
--- src/components/joy/MainText.js
+++ src/components/joy/MainText.js
@@ -6,7 +6,7 @@ const MainText = () => {
     return (
         <>
             <p className='main_text'>I enjoy playing around with <span>HTML5</span>, <span>CSS3</span>, <span>JavaScript</span>, and <span>REACT.js.</span></p>
-            <p className='main_text'> I am currently studing on<span>Node.js</span>, hopefully will not take more than a mont to be able to implement it professionally</p>
+            <p className='main_text'> I am currently studing on<span>Node.js</span>, hopefully will not take more than a mont to be able to implement it professionally.</p>
         </>)
 }
 
