8b6e325933f64ea09db819322f44118432ab5b56
emreavcia <avciemrea@gmail.com>
2022-04-16 00:54:44 +0200

    typo is corrected

--- src/components/joy/MainText.js
+++ src/components/joy/MainText.js
@@ -6,7 +6,7 @@ const MainText = ({ handleAnimation, handleAnimationMoveBack }) => {
     return (
         <>
             <p className='main_text'>I enjoy playing around with <span onMouseOut={handleAnimationMoveBack} onMouseOver={handleAnimation}>HTML5</span>, <span onMouseOver={handleAnimation} onMouseOut={handleAnimationMoveBack}>CSS3</span>, <span onMouseOut={handleAnimationMoveBack} onMouseOver={handleAnimation}>Javascript</span>, and <span onMouseOut={handleAnimationMoveBack} onMouseOver={handleAnimation}>REACT.js.</span></p>
-            <p className='main_text'> I am currently studing on <span onMouseOver={handleAnimation} onMouseOut={handleAnimationMoveBack}>Node.js</span>, hopefully will not take more than a month to be able to implement it professionally.</p>
+            <p className='main_text'> I am currently studying on <span onMouseOver={handleAnimation} onMouseOut={handleAnimationMoveBack}>Node.js</span>, hopefully will not take more than a month to be able to implement it professionally.</p>
         </>
     )
 }
